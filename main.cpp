d4adfaddf14b2a1a2a6ffba54526543db118b208
Pavel Zhigulin <zhigulin.pv@gmail.com>
2021-03-28 05:53:27 +0300

    Parser ready

--- main.cpp
+++ main.cpp
@@ -2,20 +2,34 @@
 #include <variant>
 #include <type_traits>
 #include <cctype>
+#include <memory>
 #include <algorithm>
 #include <stdexcept>
+#include <charconv>
+#include <sstream>
 #include <set>
+#include <optional>
 
-/// Guu grammar:
+/// Guu:
 /// ============
-///     program: sub (sub)*
-///     sub: SUB ID instructions_list NEWLINE
-///     instructions_list: NEWLINE instruction (NEWLINE instruction)*
-///     instruction: print | set | call
-///     print: PRINT param
-///     set: SET ID param
-///     call: CALL ID
-///     param: ID | INTEGER
+/// Where to check:
+///     https://mdkrajnak.github.io/ebnftest/
+/// Grammar:
+///     program ::= sub ('\n' (sub))*
+///     sub ::= ('\n')* SUB ' ' ID '\n' inst_list
+///     inst_list ::= (inst | inst ('\n')+ inst_list)
+///     inst ::= print | call | set
+///     print ::= PRINT ' ' param
+///     call ::= CALL ' ' ID
+///     set ::= SET ' ' ID ' ' param
+///     param ::= ID | NUM
+///     PRINT ::= "print"
+///     CALL ::= "call"
+///     SET ::= "set"
+///     SUB ::= "sub"
+///     ID ::= #"[a-zA-Z0-9]+"
+///     NUM ::= #"[0-9]+"
+
 
 // Just helper for std::visit, took from example https://en.cppreference.com/w/cpp/utility/variant/visit
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
@@ -62,6 +76,41 @@ struct Token
     std::string value_;
 };
 
+using TokenVal =
+std::variant<
+        Token<TokenType::SUB>,
+        Token<TokenType::ID>,
+        Token<TokenType::PRINT>,
+        Token<TokenType::CALL>,
+        Token<TokenType::NEWLINE>,
+        Token<TokenType::SET>,
+        Token<TokenType::INTEGER>,
+        Token<TokenType::EOL>
+>;
+
+template<TokenType TT>
+Token<TT> get(const TokenVal& val)
+{
+    return std::get< Token<TT> >(val);
+}
+
+TokenType getTokenType(const TokenVal& val)
+{
+    return std::visit([&](auto&& t) { return t.type_; }, val);
+}
+
+std::string getTokenValue(const TokenVal& val)
+{
+    return std::visit([&](auto&& t) { return t.value_; }, val);
+}
+
+template<TokenType ...TT>
+bool tokenHasType(const TokenVal& val)
+{
+    return (std::holds_alternative<Token<TT>>(val) || ...);
+}
+
+
 template<TokenType TT>
 std::ostream& operator<<(std::ostream& os, const Token<TT>& token)
 {
@@ -74,28 +123,47 @@ std::ostream& operator<<(std::ostream& os, const Token<TokenType::NEWLINE>& toke
     return os << "Token<" << token.type_ << ">(\\n)";
 }
 
+std::ostream& operator<<(std::ostream& os, const TokenVal& val)
+{
+    std::visit([&os](auto&& token){ os << token; }, val);
+    return os;
+}
+
 
 class Tokenizer
 {
 public:
-    using TokenVal =
-    std::variant<
-            Token<TokenType::SUB>,
-            Token<TokenType::ID>,
-            Token<TokenType::PRINT>,
-            Token<TokenType::CALL>,
-            Token<TokenType::NEWLINE>,
-            Token<TokenType::SET>,
-            Token<TokenType::INTEGER>,
-            Token<TokenType::EOL>
-    >;
-
     Tokenizer(std::string text)
             : text_(std::move(text))
             , currentChar_(text_.begin())
-            , prevTokenType_(TokenType::NEWLINE)
             , currLine_(1)
-    {}
+    {
+
+    }
+
+    Tokenizer(Tokenizer&& other)
+        : text_(std::string())
+        , currentChar_(std::cend(text_))
+        , currLine_(static_cast<size_t>(-1))
+    {
+        using std::swap;
+
+        if (other.isEnd())
+        {
+            swap(text_, other.text_);
+            swap(currLine_, other.currLine_);
+            currentChar_ = std::cend(text_);
+        }
+        else
+        {
+            auto pos = std::distance(std::cbegin(other.text_), other.currentChar_);
+            swap(text_, other.text_);
+            swap(currLine_, other.currLine_);
+            currentChar_ = std::cbegin(text_) + pos;
+        }
+
+    }
+
 
     TokenVal getNext()
     {
@@ -103,8 +171,7 @@ public:
 
         if (*currentChar_ == '\n')
         {
-            currLine_++;
-            currentChar_++;
+            skipNewLines();
             return construct<TokenType::NEWLINE>("\n");
         }
 
@@ -118,33 +185,44 @@ public:
             return construct<TokenType::INTEGER>(getInteger());
         }
 
-        if(std::isalpha(*currentChar_))
+        if(std::isalnum(*currentChar_))
         {
             std::string tv = advance();
 
-            if (prevTokenType_ == TokenType::NEWLINE)
+            if(tv == "sub")
             {
-                if(tv == "sub")
-                {
-                    return construct<TokenType::SUB>(tv);
-                }
-                if (tv == "print")
-                {
-                    return construct<TokenType::PRINT>(tv);
-                }
-                if(tv == "call")
-                {
-                    return construct<TokenType::CALL>(tv);
-                }
-                if(tv == "set")
-                {
-                    return construct<TokenType::SET>(tv);
-                }
-                throw std::runtime_error("Unexpected token '" + tv + "' on line " + std::to_string(currLine_));
+                return construct<TokenType::SUB>(tv);
+            }
+            if (tv == "print")
+            {
+                return construct<TokenType::PRINT>(tv);
+            }
+            if(tv == "call")
+            {
+                return construct<TokenType::CALL>(tv);
+            }
+            if(tv == "set")
+            {
+                return construct<TokenType::SET>(tv);
             }
 
             return construct<TokenType::ID>(tv);
         }
+
+        throw std::runtime_error("Unexpected symbol '" + std::string(1, *currentChar_) + "' on line " + std::to_string(currLine_));
+    }
+
+    TokenVal peekNext()
+    {
+        auto prevPos = currentChar_;
+        auto result = getNext();
+        currentChar_ = prevPos;
+        return result;
+    }
+
+    size_t currentLine() const
+    {
+        return currLine_;
     }
 
 private:
@@ -185,13 +263,23 @@ private:
 
     void skipSpaces()
     {
-        while (std::isspace(*currentChar_) && *currentChar_ != '\n' && !isEnd())
+        while (!isEnd() && std::isspace(*currentChar_) && *currentChar_ != '\n')
         {
             currentChar_++;
         }
     }
 
-    bool isEnd()
+    void skipNewLines()
+    {
+        while (*currentChar_ == '\n')
+        {
+            currentChar_++;
+            currLine_++;
+            skipSpaces();
+        }
+    }
+
+    bool isEnd() const
     {
         return currentChar_ == std::cend(text_);
     }
@@ -199,56 +287,349 @@ private:
     template<TokenType TT>
     TokenVal construct(std::string value)
     {
-        prevTokenType_ = TT;
         return Token<TT>{std::move(value)};
+    }
+
+    std::optional<char> peek()
+    {
+        return (currentChar_+1) != std::cend(text_)
+               ? std::optional<char>{*(currentChar_+1)}
+               : std::nullopt;
     }
 
 private:
     using Iterator = decltype(std::declval<std::string>().cbegin());
 
-    TokenType prevTokenType_;
     std::string text_;
     Iterator currentChar_;
     size_t currLine_;
 };
 
+using Id = std::string;
+using Integer = std::int64_t;
+
+using Param = std::variant<Id, Integer>;
+
+struct Set
+{
+    Id id_;
+    Param param_;
+};
+
+struct Call
+{
+    Id id_;
+};
+
+struct Print
+{
+    Param param_;
+};
+
+using Instruction = std::variant<Set, Call, Print>;
+
+void print(const Instruction& i, std::ostream& os, int indent)
+{
+    auto printParam = [&](auto&& p){ os << p; };
+
+    os << std::string(indent, ' ');
+    std::visit(
+            overloaded{
+                [&](const Set& s)
+                {
+                    os << "(Set id=" << s.id_ << " param=";
+                    std::visit(printParam, s.param_);
+                    os << ")";
+                },
+                [&](const Call& c)
+                {
+                    os << "(Call id=" << c.id_ << ")";
+                },
+                [&](const Print& p)
+                {
+                    os << "(Print param=";
+                    std::visit(printParam, p.param_);
+                    os << ")";
+                }
+            },
+            i
+    );
+    os << std::endl;
+}
+
+struct Sub
+{
+    size_t line_;
+    std::string name_;
+    std::vector<Instruction> instructionsList_;
+};
+
+void print(const Sub& s, std::ostream& os, int indent)
+{
+    std::cout << std::string(indent, ' ') <<  "(Sub line=" << s.line_ << " name=" << s.name_ << ")" << std::endl;
+    for(auto&& i : s.instructionsList_)
+    {
+        print(i, os, indent+2);
+    }
+}
+
+struct Program
+{
+    std::vector<Sub> subs_;
+};
+
+void printAst(const Program& prg)
+{
+    std::cout << "(Program)" << std::endl;
+
+    for(auto&& s : prg.subs_)
+    {
+        print(s, std::cout, 2);
+    }
+}
+
+class Parser
+{
+public:
+    Parser(Tokenizer t)
+        : tokenizer_(std::move(t))
+        , currToken_(tokenizer_.getNext())
+        , prevToken_(currToken_)
+    {
+    }
+
+    Program buildAST()
+    {
+        return program();
+    }
+
+    /// program ::= sub ('\n' (sub))*
+    Program program()
+    {
+        Program result;
+
+        result.subs_.emplace_back( sub() );
+
+        while( not tokenHasType<TokenType::EOL>(currToken_) )
+        {
+            result.subs_.emplace_back( sub() );
+        }
+
+        return result;
+    }
+
+    /// sub ::= ('\n')* SUB ' ' ID '\n' inst_list
+    Sub sub()
+    {
+        while (tokenHasType<TokenType::NEWLINE>(currToken_))
+        {
+            eat<TokenType::NEWLINE>();
+        }
+
+        eat<TokenType::SUB>();
+
+        Sub result;
+        result.line_ = tokenizer_.currentLine();
+        result.name_ = id();
+
+        eat<TokenType::NEWLINE>();
+        result.instructionsList_ = instructionsList();
+
+        return result;
+    }
+
+    ///     inst_list ::= (inst | inst ('\n')+ inst_list)
+    std::vector<Instruction> instructionsList()
+    {
+        std::vector<Instruction> result;
+
+        result.emplace_back( instruction() );
+
+        while (tokenHasType<TokenType::NEWLINE>(currToken_))
+        {
+            eat<TokenType::NEWLINE>();
+
+            if (not isInstruction(currToken_))
+            {
+                break;
+            }
+
+            result.emplace_back( instruction() );
+        }
+
+        return result;
+    }
+
+    /// inst ::= print | call | set
+    Instruction instruction()
+    {
+        Instruction result;
+
+        std::visit(
+                overloaded {
+                    [&](Token<TokenType::PRINT>&)
+                    {
+                        eat<TokenType::PRINT>();
+                        result = Print{ param() };
+                    },
+                    [&](Token<TokenType::CALL>&)
+                    {
+                        eat<TokenType::CALL>();
+                        result = Call{ id() };
+                    },
+                    [&](Token<TokenType::SET>&)
+                    {
+                        eat<TokenType::SET>();
+                        result = Set{ id(), param() };
+                    },
+                    [&](auto&& token)
+                    {
+                        instructionExpected(token);
+                    }
+                },
+                currToken_);
+        return result;
+    }
+
+    /// param ::= ID | NUM
+    Param param()
+    {
+        Param result;
+        std::visit(
+                overloaded{
+                    [&](Token<TokenType::ID>& token)
+                    {
+                        result = id();
+                    },
+                    [&](Token<TokenType::INTEGER>& token)
+                    {
+                        result = integer();
+                    },
+                    [&](auto&& token)
+                    {
+                        throw std::runtime_error("unexpected token " + token.value_);
+                    }
+                },
+                currToken_
+        );
+
+        return result;
+    }
+
+    /// NUM ::= #"[0-9]+"
+    Integer integer()
+    {
+        Integer result;
+        auto token = get<TokenType::INTEGER>(currToken_);
+        currToken_ = tokenizer_.getNext();
+
+        auto [p, ec] =  std::from_chars(
+                            token.value_.data(),
+                            token.value_.data() + token.value_.size(),
+                            result
+                        );
+
+        if(ec != std::errc())
+        {
+            throw std::runtime_error("Cannot cast integer");
+        }
+
+        return result;
+    }
+
+    // ID ::= #"[a-zA-Z0-9]+"
+    Id id()
+    {
+        Id value = get<TokenType::ID>(currToken_).value_;
+        currToken_ = tokenizer_.getNext();
+        return value;
+    }
+
+private:
+    bool isInstruction(const TokenVal& val)
+    {
+        return tokenHasType<TokenType::PRINT, TokenType::CALL, TokenType::SET>(currToken_);
+    }
+
+    template<TokenType TT>
+    void eat()
+    {
+        if (tokenHasType<TT>(currToken_) )
+        {
+            currToken_ = tokenizer_.getNext();
+        }
+        else
+        {
+            unexpectedToken<TT>();
+        }
+    }
+
+    template <TokenType TT>
+    void unexpectedToken()
+    {
+        std::ostringstream ss;
+        ss << "Unexpected token in line " << tokenizer_.currentLine()
+           << " [Expected = " << Token<TT>("") << ", "
+           <<   "Actual = " << currToken_ << "]";
+
+        throw std::runtime_error(ss.str());
+    }
+
+    template <TokenType TT>
+    void instructionExpected(const Token<TT>& token)
+    {
+        std::ostringstream ss;
+        ss << "Instruction token expected in line " << tokenizer_.currentLine()
+           << ", but got " << token;
+
+        throw std::runtime_error(ss.str());
+    }
+
+
+    Tokenizer tokenizer_;
+    TokenVal currToken_;
+    TokenVal prevToken_;
+};
+
+
 int main()
 {
     const std::string text =
-            "sub main\n"
-            "  set a 1\n"
+            "sub foo\n"
+            "set a 1\n"
+            "sub main\n\n\n"
+            "set a 1\n"
             "print a\n"
             "call main\n"
+            "sub walle\n"
+            "print a"
             ;
 
-    Tokenizer t(text);
-
-    bool end = false;
-    while (!end)
+    try
     {
-        try
+        std::cout << "PROGRAM:" << std::endl;
+        std::cout << text << std::endl << std::endl;
+
+        std::cout << "TOKENS:" << std::endl;
+        auto t = Tokenizer(text);
+        auto tv = t.getNext();
+        while(getTokenType(tv) != TokenType::EOL)
         {
-            auto val = t.getNext();
-            std::visit(
-                    overloaded{
-                            [&](Token<TokenType::EOL> token)
-                            {
-                                std::cout << token << std::endl;
-                                end = true;
-                            },
-                            [](auto &&token)
-                            {
-                                std::cout << token << std::endl;
-                            }
-                    }, val
-            );
+            std::cout << tv << std::endl;
+            tv = t.getNext();
         }
-        catch(const std::runtime_error& e)
-        {
-            std::cerr << "ERROR: " << e.what() << std::endl;
-            break;
-        }
+        std::cout << std::endl;
+
+        std::cout << "PARSER:" << std::endl;
+        auto p = Parser(Tokenizer(text));
+        auto res = p.buildAST();
+        printAst(res);
     }
+    catch(const std::runtime_error& e)
+    {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+    }
+
 
     return 0;
 }
